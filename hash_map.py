1ae3f24449ebedb1abf4286ee781e1011ded3a88
Samuel <semka13022007@gmail.com>
2025-02-28 22:44:31 +0300

    sdelal hash_map'u

---
+++ hash_map.py
@@ -0,0 +1,23 @@
+storage = {}
+
+hash_func = len
+
+def put(key: str, value: int) -> None:
+    hash_ = hash_func(key)
+    storage.setdefault(hash_, []).append((key, value))
+
+def get(key: str) -> tuple:
+    for i in storage.get(hash_func(key)): # не знаю насколько корректно использовать тут функцию get... но это для благих целей и быстрого поиска
+        if i[0] == key:                   # один цикл, который ищет по всем коллизиям, так что сложность O(n), где n - глубина коллизии ключа
+            return i
+    
+                
+put('Nissan', 'Skyline R34')
+put('Лаадаа', 'Калина')
+put('BMW', 'E34')
+
+nissan = get('Nissan')
+toyota = get('Toyota')
+
+print(f'Ищем ниссан: {nissan}')
+print(f'Ищем не существующую тойоту: {toyota}')
